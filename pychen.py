c6dc5b8e7e3aa45426737ee128ea1fbc997134d5
Daniel Chen <chen@daniel.im>
2014-11-07 21:42:15 -0600

    Somehow ended up with Classes and trying a find then split method to identify commands

--- pychen.py
+++ pychen.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python
-#http://wiki.shellium.org/w/Writing_an_IRC_bot_in_Python
+'''
+pyChen. IRC bot created in python for the entertainment of IRC users.
+Author: Daniel Chen
+Version: 0.0.1-Alpha
+Inspiration from: http://wiki.shelliu.org/w/Writing_an_IRC_bot_in_Python
+'''
 
 import socket
 import pyscrape
 import tldrwiki
+from chatfunc import chatFunc
 
-server = "irc.drwilco.net"
-port = 6667
-channel = "#bottest"
-botnick = "pychen"
-breakit = "\r\n"
+# Connect to server and join channel
+class connect:
+    def __init__(self, server, port, channel, botnick):
+        self.server = server
+        self.port = port
+        self.channel = channel
+        self.botnick = botnick
+        self.br = "\r\n"
+        self.ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def ping():
-    ircsock.send("PONG :Pong\n")
-def sendmsg(chan, msg):
-    ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n")
-def joinchan(chan):
-    ircsock.send("JOIN "+ chan + breakit)
-def hello():
-    ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
-def latesttldrwiki():
-    linkD = pyscrape.pyScrape()
-    pyscrape.resizeImg(linkD['pImage'])
-    txt = tldrwiki.outputOCR()
-    tldrmsg = txt +" |Date: "+ linkD['pDate'] + " |Link: " + linkD['pImage']
-    ircsock.send("PRIVMSG "+ channel +" :"+ tldrmsg + "\n")
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, port))
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :pychen by danielim"+ breakit)
-ircsock.send("NICK "+ botnick + breakit)
+        self.ircsock.connect((self.server, self.port))
+        self.ircsock.send("USER "+ self.botnick +" "+ self.botnick +" "+ self.botnick +" :pychen by danielim"+ self.br)
+        self.ircsock.send("NICK "+ self.botnick + self.br)
+        self.ircsock.send("JOIN "+ self.channel + self.br)
+    def loopy(self):
+        while 1:
+            ircmsg = self.ircsock.recv(2048)
+            ircmsg = ircmsg.strip('\n\r')
+            print(ircmsg)
+            cf = chatFunc() 
 
-joinchan(channel)
+            if ircmsg.find("PING :") != -1:
+                cf.ping(self.ircsock)
+#"if" conditional test to replace current ones. Enable finding the trigger "|" (pipe) and figure out what command is requested.
+            if ircmsg.find("|")  != -1:
+                ircarg = ircmsg.split(' ', 3)
+                # ircarg[0] = User
+                # ircarg[1] = IRC command (PRIVMSG, NICK, etc)
+                # ircarg[2] = Channel
+                # ircarg[3] = Message
+                msg = ircarg[3]
+                if msg[1:2] == "|":
+                    cmd = msg[1:] 
+                    cmd = cmd.split(' ') 
+                     
+#            if ircmsg.find(":|Hello "+ self.botnick) != -1:
+#                cf.hello(self.channel, self.ircsock)
+#        
+#            if ircmsg.find(":|tldrwiki") != -1:
+#                cf.latesttldrwiki(self.ircsock)
 
-while 1:
-    ircmsg = ircsock.recv(2048)
-    ircmsg = ircmsg.strip('\n\r')
-    print(ircmsg)
-
-    if ircmsg.find(":#Hello "+ botnick) != -1:
-        hello()
-
-    if ircmsg.find("PING :") != -1:
-        ping()
-
-    if ircmsg.find(":#tldrwiki") != -1:
-        latesttldrwiki()
+if __name__ == "__main__":
+    irc = connect("irc.drwilco.net", 6667, "#bottest", "pychen")
+    irc.loopy()
--- test.py
+++ test.py
@@ -1,38 +1,16 @@
-# Import some necessary libraries.
-import socket 
+s = ':daniel!~daniel@75-1-51-146.lightspeed.snantx.sbcglobal.net PRIVMSG #bottest :ok added test against | not being the first character'
+si = ':daniel!~daniel@75-1-51-146.lightspeed.snantx.sbcglobal.net PRIVMSG #bottest :|test'
+if '|' in s:
+    cmd1 = s.split(' ', 3)
+    if cmd1[3][1:2] == '|':
+        print "success 1"
+    else:
+        print "fail 1"
 
-# Some basic variables used to configure the bot        
-server = "irc.freenode.net" # Server
-channel = "#tamulug" # Channel
-botnick = "Mybot" # Your bots nick
-
-
-def ping(): # This is our first function! It will respond to server Pings.
-  ircsock.send("PONG :pingis\n")  
-
-def sendmsg(chan , msg): # This is the send message function, it simply sends messages to the channel.
-  ircsock.send("PRIVMSG "+ chan +" :"+ msg +"\n") 
-
-def joinchan(chan): # This function is used to join channels.
-  ircsock.send("JOIN "+ chan +"\n")
-
-def hello(): # This function responds to a user that inputs "Hello Mybot"
-  ircsock.send("PRIVMSG "+ channel +" :Hello!\n")
-                  
-ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircsock.connect((server, 6667)) # Here we connect to the server using the port 6667
-ircsock.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :This bot is a result of a tutoral covered on http://shellium.org/wiki.\n") # user authentication
-ircsock.send("NICK "+ botnick +"\n") # here we actually assign the nick to the bot
-
-joinchan(channel) # Join the channel using the functions we previously defined
-
-while 1: # Be careful with these! it might send you to an infinite loop
-  ircmsg = ircsock.recv(2048) # receive data from the server
-  ircmsg = ircmsg.strip('\n\r') # removing any unnecessary linebreaks.
-  print(ircmsg) # Here we print what's coming from the server
-
-  if ircmsg.find(":Hello "+ botnick) != -1: # If we can find "Hello Mybot" it will call the function hello()
-    hello()
-
-  if ircmsg.find("PING :") != -1: # if the server pings us then we've got to respond!
-    ping()
+if '|' in si:
+    cmd2 = si.split(' ', 3)
+    if cmd2[3][1:2] == '|':
+        print "success 2"
+    else:
+        print cmd 
+        print "fail 2"
